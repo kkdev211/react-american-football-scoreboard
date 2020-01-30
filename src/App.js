7319c4d42cc542bad5e93d127d298c9f7e0af48b
kkdev211 <kkdev211@users.noreply.github.com>
2020-01-29 22:18:29 -0500

    step 4

--- src/App.js
+++ src/App.js
@@ -33,12 +33,12 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button className="homeButtons__touchdown" onClick = {onClickHandler}>Home Touchdown</button>
+          <button className="homeButtons__fieldGoal" onClick = {() => setScoreLions(scoreLions+ 3)}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button className="awayButtons__touchdown" onClick = {onClickHandler2}>Away Touchdown</button>
+          <button className="awayButtons__fieldGoal"onClick = {() => setScoreTigers(scoreTigers + 3)}>Away Field Goal</button>
         </div>
       </section>
     </div>

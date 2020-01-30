5038b6de548859dbc2758b5c636add0b9c420393
kkdev211 <kkdev211@users.noreply.github.com>
2020-01-29 23:24:22 -0500

    having fun

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 //TODO: STEP 1 - Import the useState hook.
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-  const initialState = 0;
-  const [scoreLions, setScoreLions] =useState(initialState); 
-  const [scoreTigers, setScoreTigers] =useState(initialState); 
+  const initialStateLions = 14;
+  const initialStateTigers = 3;
+  const [scoreLions, setScoreLions] =useState(initialStateLions); 
+  const [scoreTigers, setScoreTigers] =useState(initialStateTigers); 
 
   const onClickHandler = () =>setScoreLions(scoreLions + 7);
   const onClickHandler2 = () =>setScoreTigers(scoreTigers + 7);
+
+  
+  //timer
+  // const Timer = ({ seconds }) => {
+  //   const [timeLeft, setTimeLeft] = useState(seconds);
+  //     useEffect (() => {
+  //       if (!timeLeft) return;
+  //       const intervalId = setTimeout(() => {
+  //         setTimeLeft(timeLeft -1);
+  //       }, 1000);
+
+  //       return () => clearInterval(intervalId);
+  //     }, [timeLeft]);
+
+      // return (
+      //   <div> 
+      //       <h1> {timeLeft}</h1>
+      //   </div>
+      // );
   return (
     <div className="container">
       <section className="scoreboard">
@@ -22,7 +42,7 @@ function App() {
 
             <div className="home__score"><span>{scoreLions}</span></div>
           </div>
-          <div className="timer">00:03</div>
+          <div className="timer"><span>00:03</span></div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
             <div className="away__score"><span>{scoreTigers}</span></div>
@@ -39,6 +59,10 @@ function App() {
         <div className="awayButtons">
           <button className="awayButtons__touchdown" onClick = {onClickHandler2}>Away Touchdown</button>
           <button className="awayButtons__fieldGoal"onClick = {() => setScoreTigers(scoreTigers + 3)}>Away Field Goal</button>
+        </div>
+        <div className="scoreBoardButtons">
+        <button className="downs" onClick = {onClickHandler2}>Down</button>
+        <button className="yards-to-go" onClick = {() => setScoreTigers(scoreTigers + 1)}>To Go</button>
         </div>
       </section>
     </div>
